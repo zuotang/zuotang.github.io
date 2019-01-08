5e1bc7d5cd49d4a897e9d8654dea19bc87e81a04
zuotang <zuo.tang@outlook.com>
2019-01-08 17:00:02 +0800

    修改项目名称

--- src/utils/public.js
+++ src/utils/public.js
@@ -3,11 +3,12 @@ import config from '../../config.json';
 
 // 基础路径
 export const basename = config.basename;
+export const projectname = config.projectname;
 export const username = config.username;
 // git地址
 export const github = `https://github.com/${username}`;
 // 项目地址
-export const project = `https://github.com/${username}${basename}`;
+export const project = `https://github.com/${username}/${projectname}`;
 
 // 图片地址
 export const getImg = url => {
